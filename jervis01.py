e7ab0cad3d7e1b51f64274a06519d4037168b3d2
joydipdutt007 <joydip8764896142dutta@gmail.com>
2020-03-18 22:26:20 -0700

    "next commit"

--- jervis01.py
+++ jervis01.py
@@ -53,16 +53,64 @@ def myCommand():
         query = str(input('Command: '))
     return query
 
+def operations():
+    list_1 = ["YouTube","Google","Music","Manage Folders"]
+    for i in range(len(list_1)):
+        speak(list_1[i]+'?')
 
 if __name__ == '__main__':
 
     greetMe()
-
+    speak('what do you want to do ?')
+    operations()
     speak('How may I help you?')
 
     while True:
 
         query = myCommand()
 
-        if 'ok' in query:
-            speak('Hello joy')
+        if 'YouTube' in query:
+
+            answer1 = myCommand()
+
+            webbrowser.open('https://www.youtube.com/results?search_query=' + answer1)
+            speak('Here is some results about' + answer1)
+
+        elif 'Google' in query:
+            speak('okay what do you want to search?')
+            answer1 = myCommand()
+
+            webbrowser.open('https://www.google.com/search?q='+answer1)
+            speak('Here is some results about'+answer1)
+
+        elif 'music' in query:
+            music_folder ='E:/Main Projects/Python/jervis/music/'
+            music1 = 'music1'
+            music2 = 'music2'
+            music3 = 'music3'
+            music4 = 'music4'
+            music5 = 'music5'
+            music = [music1, music2, music3, music4, music5]
+            random_music = music_folder + random.choice(music) + '.mp3'
+            os.system(random_music)
+
+            speak('Okay, here is your music! Enjoy!')
+        else:
+            query = query
+            speak('Searching...')
+            try:
+                try:
+                    res = client.query(query)
+                    results = next(res.results).text
+                    speak('Got it.')
+                    speak(results)
+
+                except:
+                    results = wikipedia.summary(query, sentences=4)
+                    speak('Got it.')
+                    speak(results)
+
+            except:
+                webbrowser.open('www.google.com')
+
+        speak('Next Command! Sir!')
